ad7288e9da7856c878f98b9403939d615a00086f
Ivet-Savcheva <ivets@ocadu.ca>
2025-03-20 22:01:01 -0400

    lab4 a.k.a. lab5

---
+++ lab5/sketch.js
@@ -0,0 +1,33 @@
+let robotModel;
+
+function preload() {
+  robotModel = loadModel('robot.obj', true);
+}
+
+function setup(){
+    createCanvas(720, 480, WEBGL);
+    describe('A model of a robot');
+
+    //divElement = createDiv('<h3>&nbsp;brick&nbsp;</h3>');
+    //divElement.position(0, 0);
+    //divElement.style('border: thin solid black; background: red');
+    //divElement.mouseOver(stopMove);
+    //divElement.mouseOut(startMove);
+
+    //createSpan('x speed');
+    //speedXSlider = createSlider(1, 10, 1, 1);
+    //createSpan('y speed');
+    //speedYSlider = createSlider(1, 10, 1, 1);
+}
+
+function draw(){
+    background ('pink');
+    //background(255);
+ 
+    // Click and drag to look around the shape
+    orbitControl();
+   
+    // This adds color to the model according to the angle of the surface
+    normalMaterial();
+    model(teapotModel);
+}
