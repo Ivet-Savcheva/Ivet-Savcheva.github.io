7eea88611789cc20654ca917ec01343997e726e2
Ivet-Savcheva <ivets@ocadu.ca>
2025-03-20 22:12:21 -0400

    update 2

--- lab5/sketch.js
+++ lab5/sketch.js
@@ -8,12 +8,6 @@ function setup(){
     createCanvas(720, 480, WEBGL);
     describe('A model of a robot');
 
-    //divElement = createDiv('<h3>&nbsp;brick&nbsp;</h3>');
-    //divElement.position(0, 0);
-    //divElement.style('border: thin solid black; background: red');
-    //divElement.mouseOver(stopMove);
-    //divElement.mouseOut(startMove);
-
     //createSpan('x speed');
     //speedXSlider = createSlider(1, 10, 1, 1);
     //createSpan('y speed');
@@ -22,12 +16,16 @@ function setup(){
 
 function draw(){
     background ('pink');
-    //background(255);
  
     // Click and drag to look around the shape
     orbitControl();
    
     // This adds color to the model according to the angle of the surface
     normalMaterial();
+    lights();
+    noStroke();
+    specularMaterial(50);
+    shininess(100);
+    rotateY(frameCount * 0.01);
     model(robotModel);
 }
